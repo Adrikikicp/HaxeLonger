ea6cd0d969f95a18bb30f52854dd4835749337f1
Adrikikicp <adrikikicp@gmail.com>
2021-11-28 16:50:56 -0500

    Eventos de HaxeLonger

---
+++ haxelonger/EventState.hx
@@ -0,0 +1,22 @@
+package haxelonger;
+
+import flixel.FlxG;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.ui.FlxUIState;
+import flixel.math.FlxRect;
+import flixel.util.FlxTimer;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import openfl.Lib;
+
+class EventState extends FlxUIState
+{
+
+    public static var EventUI;
+
+
+    
+
+
+
+}
